ddd5717a45f004b48bcb6233be24bbc6a97f2f90
pedro henrique <silvaphp1515@gmail.com>
2026-01-07 01:24:30 -0300

    Feat: Implementa mensagens de erro de validação em PT-BR.

--- GerenciadorAtivos/Models/Ativo.cs
+++ GerenciadorAtivos/Models/Ativo.cs
@@ -6,22 +6,32 @@ namespace GerenciadorAtivos.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [Required(ErrorMessage = "Por favor, digite o nome do equipamento.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo {1} caracteres.")] // {1} pega o valor 100 automaticamente
         [Display(Name = "Nome do Equipamento")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "O patrimônio é obrigatório.")]
+        [Required(ErrorMessage = "O número de patrimônio é obrigatório.")]
+        [StringLength(20, ErrorMessage = "O patrimônio deve ter no máximo {1} caracteres.")]
         public string Patrimonio { get; set; }
 
+        [Required(ErrorMessage = "Selecione o tipo do ativo.")]
         [Display(Name = "Tipo de Ativo")]
         public TipoAtivo Tipo { get; set; }
 
+        [Required(ErrorMessage = "Por favor, digite o nome da marca.")]
+        [StringLength(50, ErrorMessage = "A marca não pode exceder {1} caracteres.")]
         public string Marca { get; set; }
+
+        [Required(ErrorMessage = "Por favor, digite o nome do modelo.")]
+        [StringLength(50, ErrorMessage = "O modelo não pode exceder {1} caracteres.")]
         public string Modelo { get; set; }
 
-        [Display(Name = "Localização")]
+        [Required(ErrorMessage = "Por favor, digite o nome do Setor.")]
+        [Display(Name = "Localização / Setor")]
         public string Setor { get; set; }
 
+        [Required(ErrorMessage = "Defina o status inicial.")]
         public StatusAtivo Status { get; set; } = StatusAtivo.Disponivel;
     }
 
